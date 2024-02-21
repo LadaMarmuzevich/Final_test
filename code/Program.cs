721912be4398b5bfa84a423ad1bedb6bd3edea6b
Lada Marmuzevich <ladabeet871@gmail.com>
2024-02-21 11:44:32 +0300

    Добавили решение программы

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через запятую:");
+string[] inputArray  = Console.ReadLine().Split(',');
+
+Console.WriteLine($"Конечный массив: [{string.Join(", ", FilterArray(inputArray))}]");
+
+static string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+    foreach (string item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] newArray = new string[count];
+    int index = 0;
+    foreach (string item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            newArray[index] = item;
+            index++;
+        }
+    }
+
+    return newArray;
+}
